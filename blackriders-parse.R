a21575644b45515deaca96193053983defa05ded
solsensmith <steven.smith@edu.usafa.edu>
2019-08-01 08:15:34 -0600

    Changed "religion" back to "mind"

--- blackriders-parse.R
+++ blackriders-parse.R
@@ -9,6 +9,6 @@ objects.v <- sapply(religion.divs.ns.l, xmlValue)
 objects.v
 
 #outputs phrases with relevant word-level values
-religion.divs.ns.l <- getNodeSet(doc, "//tei:div2[@ana='#theme-religion']//tei:w[@ana='#ref-object']/ancestor-or-self::node()[position()=2]", namespaces = c(tei = "http://www.tei-c.org/ns/1.0"))
+religion.divs.ns.l <- getNodeSet(doc, "//tei:div2[@ana='#theme-mind']//tei:w[@ana='#ref-object']/ancestor-or-self::node()[position()=2]", namespaces = c(tei = "http://www.tei-c.org/ns/1.0"))
 objects.v <- sapply(religion.divs.ns.l, xmlValue)
 objects.v
