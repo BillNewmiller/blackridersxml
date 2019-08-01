95b42e6f53f6270f47e5f5d9ee2c0b27f42529a3
solsensmith <steven.smith@edu.usafa.edu>
2019-08-01 06:19:01 -0600

    BR parsing code from May.

---
+++ blackriders-parse.R
@@ -0,0 +1,14 @@
+library(XML)
+options(max.print = .Machine$integer.max)
+doc <- xmlTreeParse("blackriders_updated.xml", useInternalNodes=TRUE)
+doc
+#outputs word-level values
+religion.divs.ns.l <- getNodeSet(doc, "//tei:div2[@ana='#theme-religion']//tei:w[@ana='#ref-object']", namespaces = c(tei = "http://www.tei-c.org/ns/1.0"))
+religion.divs.ns.l
+objects.v <- sapply(religion.divs.ns.l, xmlValue)
+objects.v
+
+#outputs phrases with relevant word-level values
+religion.divs.ns.l <- getNodeSet(doc, "//tei:div2[@ana='#theme-religion']//tei:w[@ana='#ref-object']/ancestor-or-self::node()[position()=2]", namespaces = c(tei = "http://www.tei-c.org/ns/1.0"))
+objects.v <- sapply(religion.divs.ns.l, xmlValue)
+objects.v
